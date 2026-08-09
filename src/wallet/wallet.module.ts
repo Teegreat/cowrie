7ca783f3a386c3f49cdebcd24d0f1df8b1121bd7
Teegreat <toyinatofarati64@gmail.com>
2026-08-09 11:53:49 +0100

    fix(wallet): update WithdrawalRepository to use PrismaWithdrawalRepository

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -14,6 +14,7 @@ import { BaasWebhookController } from './interface/baas-webhook.controller';
 import { DepositRepository } from './application/ports/deposit-repository.port';
 import { PrismaDepositRepository } from './infrastructure/persistence/prisma-deposit.repository';
 import { WithdrawalRepository } from './application/ports/withdrawal-repository.port';
+import { PrismaWithdrawalRepository } from './infrastructure/persistence/prisma-withdrawal.repository';
 import { ProcessDepositUseCase } from './application/use-cases/process-deposit.use-case';
 import { InitiateWithdrawalUseCase } from './application/use-cases/initiate-withdrawal.use-case';
 import { AttemptExternalTransferUseCase } from './application/use-cases/attempt-external-transfer.use-case';
@@ -31,7 +32,7 @@ import { ReleaseWithdrawalUseCase } from './application/use-cases/release-withdr
       useClass: PrismaVirtualAccountRepository,
     },
     { provide: DepositRepository, useClass: PrismaDepositRepository },
-    { provide: WithdrawalRepository, useClass: PrismaWalletRepository },
+    { provide: WithdrawalRepository, useClass: PrismaWithdrawalRepository },
     CreateWalletUseCase,
     GetWalletUseCase,
     CreateVirtualAccountUseCase,
